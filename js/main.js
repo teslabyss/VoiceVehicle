48942c695d5a47f189d61c9b52fc053a5bd26240
teslabyss <34561948+teslabyss@users.noreply.github.com>
2025-05-22 09:54:59 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   function sendToAPI(message) {
-    fetch("http://localhost/A-exm-u3-api/endpoints/chat.php", {
+    fetch("http://44.220.172.127/A-exm-u3-api/endpoints/chat.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function saveStatusToDB(status) {
     const username = document.getElementById("username").value || "Anónimo";
 
-    fetch("http://localhost/A-exm-iot-api/controllers/AddIotDevice.php", {
+    fetch("http://44.220.172.127/A-exm-iot-api/controllers/AddIotDevice.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
